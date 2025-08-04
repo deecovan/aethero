09c41b5a59555a76606b5929ed1a9e754f8334e9
Dee Covan aka dcheva, cheva and more <dee.covan@gmail.com>
2025-08-04 18:34:50 +0300

    Format cat2pipe.c

--- C/karnigan/cat2pipe.c
+++ C/karnigan/cat2pipe.c
@@ -14,91 +14,87 @@
 
 long cnt, cc, nn, ss, tt, cw, cwi, ccc;
 int c, i, j, k, charid, longspaces, inword, words, addlastword;
-int nwords[MAXCVI+1], nchars[MAXFRQ];
+int nwords[MAXCVI + 1], nchars[MAXFRQ];
 char str[2];
 
 void main() {
+    for (cnt = 0; (c = getchar()) != EOF; ++cnt) {
+        if (c == CSPACE) {
+            if (longspaces != 1) {
+                ++ss;
+                putchar(c);
+                longspaces = 1;
+                inword = 0;
+            }
+        }
 
-  for (cnt = 0; (c = getchar()) != EOF; ++cnt) {
+        else if (c == CTAB) {
+            if (longspaces != 1) {
+                ++tt;
+                putchar(c);
+                longspaces = 1;
+                inword = 0;
+            }
+        }
 
-    if(c == CSPACE) {
-      if(longspaces != 1) {
-        ++ss;
-        putchar(c);
-        longspaces = 1;
-        inword = 0;
-      }
+        else if (c == CNEW || c == CRET) {
+            ++nn;
+            /* C++ style comments are not allowed in ISO C90 // printf("%ld", nn); */
+            putchar(c);
+            longspaces = 0;
+            inword = 0;
+        }
+
+        else {
+            ++cc;
+            ++nchars[c - 'A'];
+            putchar(c);
+            longspaces = 0;
+            if (inword != 1) {
+                ++cw;
+                inword = 1;
+                addlastword = 1;
+                cwi = 1;
+            } else {
+                ++cwi;
+            }
+        }
+
+        if (addlastword == 1 && inword != 1 && cwi > 0 || c == EOF) {
+            addlastword = 0;
+            if (cwi > MAXCVI) {
+                ++nwords[MAXCVI];
+            } else {
+                ++nwords[cwi];
+            }
+        }
     }
 
-    else if(c == CTAB) {
-      if(longspaces != 1) {
-        ++tt;
-        putchar(c);
-        longspaces = 1;
-        inword = 0;
-      }
-    }
-
-    else if(c == CNEW || c == CRET){
-      ++nn;
-      /* C++ style comments are not allowed in ISO C90 // printf("%ld", nn); */
-      putchar(c);
-      longspaces = 0;
-      inword = 0;
-    }
-
-    else {
-      ++cc;
-      ++nchars[c-'A'];
-      putchar(c);
-      longspaces = 0;
-      if(inword != 1) {
-        ++cw;
-        inword = 1;
-		addlastword = 1;
-        cwi = 1;
-      } else {
-        ++cwi;
-      }
-    }
-
-    if(addlastword == 1 && inword !=1 && cwi > 0 || c == EOF) {
-      addlastword = 0;
-      if(cwi > MAXCVI) {
-        ++nwords[MAXCVI];
-      } else {
-        ++nwords[cwi];
-      }
-    }
-
-  }
-
-  printf("\n\n");
-  printf("Input symbols: %ld, Output Chars: %ld, Spaces: %ld, Tabs: %ld", cnt, cc, ss, tt);
-  printf("\n");
-  printf("Words: %ld, Lines: %ld", cw, nn);
-  printf("\n");
-
-  printf("\nWord lengths from 1 to %d+:", MAXCVI);
-  for(i = 0; i < ((MAXCVI) / 10); ++i){
+    printf("\n\n");
+    printf("Input symbols: %ld, Output Chars: %ld, Spaces: %ld, Tabs: %ld", cnt, cc, ss, tt);
     printf("\n");
-    for(j = 1; j < 11; ++j){
-      k = i * 10 + j;
-      printf("(%2d)[%2d] ",k, nwords[k]);
-      ccc = ccc + nwords[k];
-    }
-  }
-  printf("\nWords lengths counted: %d\n", ccc);
-
-  printf("\nA-z Frequencies from A(0) to ?(%d):\n", MAXFRQ - 1);
-  for(i = 0; i < (MAXFRQ / 8); ++i){
-    for(j = 0; j < (MAXFRQ / (MAXFRQ / 8)); ++j){
-      k = i * 8 + j;
-	  str[0] = (char)(k +FIRSTLETTER);
-	  str[1] = '\0';
-      printf("(%3s)[%3d] ", str , nchars[k]);
-    }
+    printf("Words: %ld, Lines: %ld", cw, nn);
     printf("\n");
-  }
+
+    printf("\nWord lengths from 1 to %d+:", MAXCVI);
+    for (i = 0; i < ((MAXCVI) / 10); ++i) {
+        printf("\n");
+        for (j = 1; j < 11; ++j) {
+            k = i * 10 + j;
+            printf("(%2d)[%2d] ", k, nwords[k]);
+            ccc = ccc + nwords[k];
+        }
+    }
+    printf("\nWords lengths counted: %d\n", ccc);
+
+    printf("\nA-z Frequencies from A(0) to ?(%d):\n", MAXFRQ - 1);
+    for (i = 0; i < (MAXFRQ / 8); ++i) {
+        for (j = 0; j < (MAXFRQ / (MAXFRQ / 8)); ++j) {
+            k = i * 8 + j;
+            str[0] = (char)(k + FIRSTLETTER);
+            str[1] = '\0';
+            printf("(%3s)[%3d] ", str, nchars[k]);
+        }
+        printf("\n");
+    }
 }
-
